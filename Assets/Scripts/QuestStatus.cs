ce0851fcac003519921a3507be786f297af56f85
NekOoTRIGGER <harquel.stephane@hotmail.fr>
2025-05-07 22:47:11 +0200

    adding couroutine for quest

--- Assets/Scripts/QuestStatus.cs
+++ Assets/Scripts/QuestStatus.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -13,17 +14,23 @@ public class QuestStatus : MonoBehaviour
 
     public void QuestAccepted()
     {
-        TextComponent.enabled = true;
-        TextComponent.text = "Quête Acceptée";
+        StopAllCoroutines(); // Optionnel : arrêter les messages précédents
+        StartCoroutine(DisplayMessage("Quête Acceptée"));
     }
+
     public void QuestFinish()
     {
-        TextComponent.enabled = true;
-        TextComponent.text = "Quête Terminée";
+        StopAllCoroutines(); // Optionnel
+        StartCoroutine(DisplayMessage("Quête Terminée"));
     }
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator DisplayMessage(string message)
     {
-        
+        TextComponent.text = message;
+        TextComponent.enabled = true;
+
+        yield return new WaitForSeconds(3f); // attendre 3 secondes
+
+        TextComponent.enabled = false;
     }
 }
